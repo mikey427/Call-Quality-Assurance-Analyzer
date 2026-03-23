882b25db81696f79117a3a622656a7d19b1f7612
Michael Baez <mrbaez427@gmail.com>
2026-03-23 00:26:45 +0000

    Initialized Svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import heroImg from './assets/hero.png'
+  import Counter from './lib/Counter.svelte'
+  import { Router, Link, Route } from "svelte-routing";
+
+  export let url = "";
+</script>
+
+
+<nav>
+  <Router {url}>
+    <Route path="/" component=""/>
+  </Router>
+</nav>
