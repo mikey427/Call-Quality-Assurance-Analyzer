5e796acac6b6975d69836a365923f56974496b30
Michael Baez <mrbaez427@gmail.com>
2026-03-31 06:47:54 +0000

    Created closure function for llmNode for systemPrompt parameter

--- backend/lib/ai/nodes/llmCall.ts
+++ backend/lib/ai/nodes/llmCall.ts
@@ -1,15 +1,12 @@
 import { SystemMessage } from "@langchain/core/messages";
 import { modelWithTools } from "../model";
 
-export const llmCall = async (state: any) => {
-  const response = await modelWithTools.invoke([
-    new SystemMessage(
-      "You are a helpful assistant tasked with performing arithmetic on a set of inputs."
-    ),
-    ...state.messages,
-  ]);
-  return {
-    messages: [response],
-    llmCalls: 1,
-  };
-};
+function createNode (systemPrompt: string) {
+	return async (state: any) => {
+		const response = await modelWithTools.invoke([
+			new SystemMessage(systemPrompt),
+			...state.messages,
+		])
+		return { messages: [response ]};
+	}
+}
